3c775950fafe7c5b3e09975695d8a67abfc2a3f4
Ubiratan Tavares <ust1973@gmail.com>
2025-04-21 21:59:20 -0300

    implementação do modulo contador.v

--- contador.v
+++ contador.v
@@ -8,6 +8,15 @@ module Contador (
     output reg [7:0] saida
 );
 
-// Insira seu código aqui
-
+	always @(posedge clk or negedge rst_n) begin
+		if (!rst_n) begin
+ 			saida = 8'h6A;
+		end else begin
+			case ({acrescer, decrecer})
+				2'b10: saida = saida + 1;
+				2'b01: saida = saida - 1;
+				default: saida = saida;
+			endcase
+		end
+	end
 endmodule
